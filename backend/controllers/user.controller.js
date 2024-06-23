054da74c8052ed55c638114d1e7758c87326a8ab
amansharma-exe <abbysharma020@gmail.com>
2024-06-23 18:13:54 +0530

    added user modals and controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,8 +1,11 @@
 import { User } from "../models/user.model";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
     const [fullname, email, password] = req.body;
+    //check null values if any
     if (!fullname || !email || !password) {
       return res
         .status(400)
@@ -11,18 +14,77 @@ export const register = async (req, res) => {
 
     const user = await User.findOne({ email });
     if (user)
-      return res
-        .status(400)
-        .json({
-          message: "User already exists with this email",
-          success: false,
-        });
+      return res.status(400).json({
+        message: "User already exists with this email",
+        success: false,
+      });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const profilePic = `https://avatar.iran.liara.run/public`;
 
     await user.create({
       fullname,
       email,
-      password,
-    })
+      password: hashedPassword,
+      profilePhoto: profilePic,
+    });
 
-  } catch (error) {}
+    return res.status(201).json({
+      message: "User account created successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const [email, password] = req.body;
+
+    //check null values if any
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "All fields are required", success: false });
+    }
+
+    //check if the email is present in the database
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Incorrect username or password", success: false });
+    }
+
+    //check if the password is matching return true or false
+    const matchPassword = await bcrypt.compare(password, user.password);
+
+    if (!matchPassword) {
+      return res
+        .status(401)
+        .json({ message: "Incorrect username or password", success: false });
+    }
+
+    const tokenData = {
+      userId: user._id,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .json({ message: `${user.fullname} logged in successfully`, user });
+  } catch (error) {
+    console.log(error);
+  }
 };
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
-import express from  "express";
+import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/ConnectDB.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/user.route.js"
 
-dotenv.config({})
-
+dotenv.config({});
 connectDB();
-
+const PORT = 8080;
 const app = express();
 
 //testing api call
@@ -14,10 +15,20 @@ const app = express();
 })
 */
 
+//middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cookieParser);
 
+const corsOptions = {
+  origin:'http://localhost:3000/',
+  credentials:true
+}
+app.use(cors(corsOptions));
 
-const PORT = 8080;
+// routes
+app.use("api/v1.user",userRoute)
 
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
-})
+});
