cb141fddb6b77101d1a81ba200c5d377a30bead7
Aman Sharma <53836272+amansharma-exe@users.noreply.github.com>
2024-06-26 20:49:15 +0530

    Update Login.jsx

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import toast from 'react-hot-toast';
 import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom'
-import { setAuthUser } from '../redux/appSlice';
+import { setAuthUser } from '../Redux/AppSlice';
 
 const Login = () => {
 
