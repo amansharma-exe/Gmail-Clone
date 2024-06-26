6b9e0f3edc375b2a8f77167c1fdd847850c345d0
Aman Sharma <53836272+amansharma-exe@users.noreply.github.com>
2024-06-26 20:29:41 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from "react-redux";
-import store from './redux/store.js';
+import Store from './Redux/Store.js';
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from "redux-persist"
 
